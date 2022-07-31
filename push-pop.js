bb9c8fa5fbc4c3a6720eb8ce97f8752aa4ab9a17
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-01 01:39:16 +0600

    push pop done

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+var numbers = [44, 56, 75, 65, 99];
+numbers.push(11);
+// console.log(numbers);
+
+// use push to add element to an array as the last element array
+var fnd = ['sabbir', 'badon', 'monir'];
+fnd.push('hridoy');
+fnd.push('anamul');
+// console.log(fnd);
+
+// use pop to get last element.
+console.log(numbers);
+numbers.pop();
+// console.log(numbers);
+
+//
+var element = numbers.pop();
+console.log(element);
