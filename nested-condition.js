f00d94028f9004381871a9f03d305d591f2ab921
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-01 11:46:41 +0600

    summary

---
+++ nested-condition.js
@@ -0,0 +1,19 @@
+var math = true;
+var geometry = true;
+var staightlint = false;
+if (math == true) {
+    if (geometry == true) {
+        if (staightlint == true) {
+            
+        }
+        else {
+            console.log('baka potha cholta hoba');
+        }
+    }
+    else {
+        console.log('pithagoras goita parba na');
+    }
+}
+else {
+    console.log(' tumi kisu i paro  na');
+}
---
+++ summary.js
@@ -0,0 +1,15 @@
+var names = ['sabbir', 'badun', 'monir', 'hridoy'];
+console.log(names[2]);
+// set explore
+names[2] = 'anmul';
+// position
+var index = names.indexOf('badun');
+
+// push
+// pop
+
+// conditional
+
+// <, >, ==, !=, <=, >=
+// && , ||
+// if, else, else if, else 
