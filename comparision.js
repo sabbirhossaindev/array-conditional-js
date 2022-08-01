d6adb70942439f87076f1d3a7ab231d53cbcc4ed
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-01 10:23:19 +0600

    conditional

--- comparision.js
+++ comparision.js
@@ -7,5 +7,15 @@ console.log(6 != 6);
 console.log(6 <= 6);
 console.log(6 >= 6);
 console.log(6 !== 6);
-// && sign dabble 
+// && sign dabble
 // || sing oth0ba/ OR
+
+// multiple comparison
+
+var money1 = 100;
+var money2 = 50;
+console.log(money1 > money2 && result1 && hight1 > hight2);
+
+var money1 = 100;
+var money2 = 50;
+console.log(money1 > money2 || result1 || hight1 > hight2);
---
+++ conditional.js
@@ -0,0 +1,17 @@
+var isApple = 100;
+var myAmount = 1080;
+if (isApple < myAmount) {
+    console.log('ok i am buy the apple');
+}
+else {
+    console.log('sorry');
+}
+
+// var isApple = 100;
+// var myAmount = 50;
+// if (isApple <= myAmount) {
+//     console.log('ok i am buy the apple');
+// }
+// else {
+//     console.log('sorry');
+// }
