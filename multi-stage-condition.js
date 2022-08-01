a05884ba7efa3a5ea8cd36876e58f9d27f116b0c
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-01 11:22:50 +0600

    multi-stage-condition

---
+++ multi-stage-condition.js
@@ -0,0 +1,16 @@
+var amount = 10;
+var danisPrice = 40;
+var buterbonPrice = 30;
+var tostPrice = 20;
+if(danisPrice < amount) {
+    console.log('danis dan');
+}
+else if (buterbonPrice < amount) {
+    console.log("buterbon dan");
+}
+else if (tostPrice < amount) {
+    console.log("tost dan");
+}
+else {
+    console.log('kali sa is beter');
+}
---
+++ multiple-conditions.js
@@ -0,0 +1,51 @@
+// var isDiploma = true;
+// var salary = 20000;
+// if (isDiploma == true) {
+//     console.log('aso beya kori');
+// }
+// else (
+//     console.log('tor kopala beya nai')
+// )
+
+// var isDiploma = true;
+// var salary = 750000;
+// if (isDiploma == true && salary > 50000) {
+//     console.log('aso beya kori');
+// }
+// else (
+//     console.log('tor kopala beya nai')
+// )
+
+
+// sob condtion full fill kora lagba
+// var isDiploma = true;
+// var salary = 750000;
+// var cars = 1;
+// if (isDiploma == true && salary > 50000 && cars >= 1) {
+//     console.log('aso beya kori');
+// }
+// else (
+//     console.log('tor kopala beya nai')
+// )
+
+
+// minimam 1 ta condition hoila hoba ||
+// var isDiploma = false;
+// var salary = 61000;
+// var cars = 0;
+// if (isDiploma === true || salary > 50000 || cars >= 1) {
+//     console.log('aso prem kori');
+// }
+// else (
+//     console.log('tomer friend er number daw')
+// )
+
+var isDiploma = false;
+var salary = 61000;
+var cars = 0;
+if (isDiploma === true && (salary > 50000 || cars >= 1)) {
+    console.log('aso prem kori');
+}
+else (
+    console.log('tomer friend er number daw')
+)
