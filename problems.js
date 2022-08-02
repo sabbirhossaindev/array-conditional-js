c2ebd5e4eb474bcce3d31e08911b38690016a791
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-02 08:20:03 +0600

    Problem done

--- problems.js
+++ problems.js
@@ -6,3 +6,33 @@ fruits.pop();
 console.log(fruits);
 fruits.push('Watermelon');
 console.log(fruits);
+
+
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
+
+if (num1 > num3) {
+    console.log(num1);
+}
+else if (num1 > num2) {
+    console.log(num1);
+}
+else if (num2 > num3) {
+    console.log(num2);
+}
+else {
+    console.log(num3);
+}
+
+
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+    console.log('Isosceles => two side equal');
+}
+else {
+    'Isosceles => two side Not equal';
+}
